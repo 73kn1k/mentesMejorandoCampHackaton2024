75ac6ef3217f50b213b98902ffa48c6c6a5d5c79
73kn1k <80842155+73kn1k@users.noreply.github.com>
2024-07-23 20:08:25 -0400

    Update MentesMejorando.sol
    Cambiado tocenCounter por unit256 para compatibilizar con el erc721.
    Agregado el set de _owner para poder implementar el setProperty.

--- MentesMejorando.sol
+++ MentesMejorando.sol
@@ -1,53 +1,50 @@
-     // SPDX-License-Identifier: MIT
-     pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-     import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-     contract MentesMejorando is ERC721
+contract MentesMejorando is ERC721
      {
-     uint public tokenCounter;
+     uint256 public tokenCounter = 0;
 
      string public theSender;
 
-          struct NFTMetadata  
-          {
-               string imageURL;
-               string walletId;
-               string kitAmount;
-          }
-
-          mapping(uint => NFTMetadata) private nfts;
-
-          // This creates a public mapping with the key being the token ID (uint) and 
-          // the value being the owner's address (address).
-          mapping(uint256 => address) private _owners;
-
-          constructor() ERC721("MentesMejorando", "MMARG")
-          {
-               tokenCounter = 0;
-               nfts[tokenCounter] = NFTMetadata({ imageURL: "https://fundacionoidmortales.org/wp-content/uploads/sites/15/2024/06/Libro-Mentes-Mejorando.png", walletId: theSender, kitAmount: "1" }); 
-          }
-
-          function createCollectible() public returns (uint) 
-          {
-               _safeMint(msg.sender, tokenCounter);
-               
-               tokenCounter++;
-               
-               return tokenCounter;
-          }
-
-          function setProperty(string memory newWalletId) public 
-          {
-               require(_owners[tokenCounter] == msg.sender, "You are not the owner of this NFT");
-               
-               nfts[tokenCounter].walletId = newWalletId;
-          }
-
-          function getProperty(uint tokenId) public view returns (string memory, string memory)
-          {
-               require(_owners[tokenId] != address(0), "Token does not exist");
-               
-               return (nfts[tokenId].walletId, nfts[tokenId].kitAmount);
-          }
+     struct NFTMetadata  
+     {
+          string imageURL;
+          string walletId;
+          string kitAmount;
      }
+
+     mapping(uint256 => NFTMetadata) private nfts;
+
+     mapping(uint256 => address) private _owners;
+
+     constructor() ERC721("MentesMejorando", "MMARG")
+     {
+          // tokenCounter = 0;
+     }
+
+     function createCollectible() public returns (uint256) 
+     {
+          tokenCounter++;
+          nfts[tokenCounter] = NFTMetadata({ imageURL: "https://fundacionoidmortales.org/wp-content/uploads/sites/15/2024/06/Libro-Mentes-Mejorando.png", walletId: theSender, kitAmount: "1" }); 
+          _safeMint(msg.sender, tokenCounter);
+          _owners[tokenCounter] = msg.sender;
+          return tokenCounter;
+     }
+
+     function setProperty(string memory newWalletId) public 
+     {
+          require(_owners[tokenCounter] == msg.sender, "You are not the owner of this NFT");
+          
+          nfts[tokenCounter].walletId = newWalletId;
+     }
+
+     function getProperty(uint256 tokenId) public view returns (string memory, string memory)
+     {
+          require(_owners[tokenId] != address(0), "Token does not exist");
+          
+          return (nfts[tokenId].walletId, nfts[tokenId].kitAmount);
+     }
+}
